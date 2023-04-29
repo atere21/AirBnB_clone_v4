aac0ff9e0f0795d866b892e3bbc252433d88df39
Jay stones <Joelpaul345@gmail.com>
2023-04-29 16:41:48 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,8 @@
 """ init blueprint """
 from flask import Blueprint
 
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -9,8 +11,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
-
-app_views = Blueprint('app_views', __name__,
-                      url_prefix='/api/v1')
-''' Blueprint for Airbnb clone API'''
